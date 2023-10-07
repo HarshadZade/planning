931d6bda5ed35691684fe16e8577ed5b69cf4780
Harshad Zade <zade@jpl.nasa.gov>
2023-10-07 13:42:58 -0400

    Working./a.out map9.txt

--- 16782-HW1/code/planner.cpp
+++ 16782-HW1/code/planner.cpp
@@ -29,45 +29,17 @@ struct State
   int g;          // Cost from the start node to this node
   int h;          // Heuristic (e.g., Euclidean distance to the target)
   State* parent;  // Pointer to the parent state
-
-  // Define a custom equality operator for the State class
-  bool operator==(const State& s) const
-  {
-    return (x == s.x && y == s.y);
-  }
+  bool has_been_visited = false;
 };
 
 // Define a comparison function for the priority queue
 struct CompareStates
 {
-  bool operator()(const State* a, const State* b) const
+  bool operator()(const State a, const State b) const
   {
-    return (a->g + a->h) > (b->g + b->h);
+    return (a.g + a.h) > (b.g + b.h);
   }
 };
-
-// Define a custom hash function for State class
-// template<> struct std::hash<State>
-// {
-//   size_t operator()(const State s) const noexcept
-//   {
-//     return std::hash<int>()(s.x) ^ std::hash<int>()(s.y);
-//     // return s->x * 5000 + s->y;
-//   }
-// };
-
-struct StateHash
-{
-  size_t operator()(const State s) const
-  {
-    return std::hash<int>()(s.x) ^ std::hash<int>()(s.y);
-    // return s->x * 5000 + s->y;
-  }
-};
-
-// Create an unordered set to store the states in the open list
-// std::unordered_set<State, StateHash> open_set;
-// std::unordered_set<State, std::hash<State>> open_set;
 
 static int flag = 0;
 
@@ -83,35 +55,34 @@ void planner(int* map, int collision_thresh, int x_size, int y_size, int robotpo
              int* target_traj, int targetposeX, int targetposeY, int curr_time, int* action_ptr)
 {
   // Create an open list (priority queue) for states to explore
-  static std::priority_queue<State*, std::vector<State*>, CompareStates> open_list;
+  static std::priority_queue<State, std::vector<State>, CompareStates> open_list;
 
   // Create a closed list to keep track of explored states
   static std::vector<std::vector<bool>> closed_list(x_size, std::vector<bool>(y_size, false));
 
-  static std::unordered_set<State, StateHash> open_set;
+  // static std::vector<std::vector<bool>> visited_list(x_size, std::vector<bool>(y_size, false));
+  static std::vector<std::vector<State>> open_list_state(x_size, std::vector<State>(y_size));
 
   // Initialize the start state just once
-  static State* start = new State{ 0, 0, 0, 0, nullptr };
+  static State start = State{ 0, 0, 0, 0, nullptr };
   if (flag == 0)
   {
-    start->x = robotposeX;
-    start->y = robotposeY;
+    start.x = robotposeX;
+    start.y = robotposeY;
+    start.has_been_visited = true;
     open_list.push(start);
-    open_set.insert(*start);
+    open_list_state[robotposeX - 1][robotposeY - 1] = start;
     flag = 1;
   }
 
   // Initialize the goal state
   int goalposeX = target_traj[target_steps - 1];
   int goalposeY = target_traj[target_steps - 1 + target_steps];
-  State* goal = new State{ goalposeX, goalposeY, 0, 0, nullptr };
-  // print goal x and y
-  // std::cout << "goal: " << goal->x << " " << goal->y << std::endl;
+  State goal = State{ goalposeX, goalposeY, 0, 0, nullptr };
 
   auto get_action = [&] {
     if (robot_traj.empty())
     {
-      std::cout << "robot_traj empty" << std::endl;
       int newx = robotposeX;
       int newy = robotposeY;
       action_ptr[0] = newx;
@@ -121,10 +92,8 @@ void planner(int* map, int collision_thresh, int x_size, int y_size, int robotpo
     int newx = robot_traj.back().first;
     int newy = robot_traj.back().second;
     robot_traj.pop_back();
-    std::cout << "newx: " << newx << " newy: " << newy << std::endl;
     action_ptr[0] = newx;
     action_ptr[1] = newy;
-    std::cout << "action: " << action_ptr[0] << " " << action_ptr[1] << std::endl;
     return;
   };
 
@@ -132,54 +101,36 @@ void planner(int* map, int collision_thresh, int x_size, int y_size, int robotpo
   action_ptr[1] = robotposeY;
 
   // Lambda function to backtrack the optimal path
-  auto backtrack_path = [&](State* goal) {
+  auto backtrack_path = [&](State goal) {
     // Backtrack to extract the optimal path
-    State* temp = goal;
-    while (temp->parent != nullptr)
+    State temp = goal;
+    // std::cout << "backtrack_path" << std::endl;
+    while (temp.parent != nullptr)
     {
       // store the whole path in a desired robot trajectory
-      robot_traj.push_back(std::make_pair(temp->x, temp->y));
-      temp = temp->parent;
+      robot_traj.push_back(std::make_pair(temp.x, temp.y));
+      temp = *temp.parent;
     }
-    std::cout << "robot_traj size: " << robot_traj.size() << std::endl;
     return;
   };
 
   if (is_planning_done)
   {
-    std::cout << "getting action" << std::endl;
     get_action();
     return;
   }
 
-  // State* test_current = open_list.top();
-  // if (test_current->x == goal->x && test_current->y == goal->y)
-  // {
-  //   std::cout << "Found plan!" << std::endl;
-  //   is_planning_done = true;
-  //   backtrack_path(test_current);
-  //   get_action();
-  //   return;
-  // }
-
   while (!open_list.empty())
   {
     // Pop the state with the lowest cost from the open list
-    State* current = open_list.top();
+    State current = open_list.top();
     open_list.pop();
-    open_set.erase(*current);
-
-    // print x and y in current
-    std::cout << "current: " << current->x << " " << current->y << std::endl;
-
-    // std::cout << "current: " << current->x << " " << current->y << std::endl;
+    open_list_state[current.x - 1][current.y - 1] = current;
     // Add the current state to the closed list
-    closed_list[current->x - 1][current->y - 1] = true;  // FIXME: Indexing is incorrect here?
-    // closed_list[current->x ][current->y] = true;  // FIXME: Indexing is incorrect here?
+    closed_list[current.x - 1][current.y - 1] = true;
     // Check if the current state is the goal state
-    if (current->x == goal->x && current->y == goal->y)
+    if (current.x == goal.x && current.y == goal.y)
     {
-      std::cout << "Found plan!" << std::endl;
       is_planning_done = true;
       backtrack_path(current);
       get_action();
@@ -189,9 +140,8 @@ void planner(int* map, int collision_thresh, int x_size, int y_size, int robotpo
     // Expand the current state
     for (int dir = 0; dir < NUMOFDIRS; dir++)
     {
-      int newx = current->x + dX[dir];
-      int newy = current->y + dY[dir];
-
+      int newx = current.x + dX[dir];
+      int newy = current.y + dY[dir];
       // Check if the new state is within the map
       if (newx >= 1 && newx <= x_size && newy >= 1 && newy <= y_size)
       {
@@ -199,60 +149,29 @@ void planner(int* map, int collision_thresh, int x_size, int y_size, int robotpo
         int cell_cost = (int)map[GETMAPINDEX(newx, newy, x_size, y_size)];
         if ((cell_cost >= 0) && (cell_cost < collision_thresh))
         {
-          // print cell cost
-          // std::cout << "cell cost: " << cell_cost << std::endl;
           // Check if the new state is in the closed list
           if (!closed_list[newx - 1][newy - 1])
-          // if (!closed_list[newx][newy])
           {
             // Make a new state for the new state
-            State new_state = State{ newx, newy, current->g + cell_cost, 0, current };
+            State new_state = State{ newx, newy, current.g + cell_cost, 0, nullptr };
+            new_state.parent = &open_list_state[current.x - 1][current.y - 1];
 
             // Compute the heuristic for the new state. Diagonal distance is used here.
-            new_state.h = MAX(abs(new_state.x - goal->x), abs(new_state.y - goal->y));
-            // new_state.h = sqrt(pow((new_state.x - goal->x),2) + pow((new_state.y - goal->y),2));
+            new_state.h = MAX(abs(new_state.x - goal.x), abs(new_state.y - goal.y));
+            // new_state.h = sqrt(pow((new_state.x - goal.x),2) + pow((new_state.y - goal.y),2));
 
             // Check if the new state is in the open list
-            bool in_open_list = open_set.find(new_state) != open_set.end();
-            if (in_open_list)
-            {
-              // std::cout << "in open list" << std::endl;
-              // std::cout << "new_state: " << new_state.x << " " << new_state.y << std::endl;
-              // If the new state is in the open list, update the cost and parent if necessary
-              State existing_state = *open_set.find(new_state);
-              // print x and y in existing state
-              // std::cout << "existing_state: " << existing_state.x << " " << existing_state.y << std::endl;
-
-              // print g and h
-              // std::cout << "Before g: " << existing_state.g << " h: " << existing_state.h << std::endl;
-              // // print g and h of new state
-              // std::cout << "new_state g: " << new_state.g << " h: " << new_state.h << std::endl;
-              if (new_state.g < existing_state.g)
-              {
-                existing_state.g = new_state.g;
-                existing_state.parent = new_state.parent;
-                // {
-                //   // State test_state = *open_set.find(new_state);
-                //   // print g and h
-                //   // std::cout << "After g: " << test_state.g << " h: " << test_state.h << std::endl;
-                //   // std::cout << "------------------------------\n";
-                // }
-                open_set.erase(existing_state);
-                open_set.insert(new_state);
-                //  find an update new_state in open_list
-                open_list.push(&new_state);
-              }
-            }
-            else
+            bool in_open_list = open_list_state[new_state.x - 1][new_state.y - 1].has_been_visited;
+            if (!in_open_list)
             {
               // Add the new state to the open list and the open set
-              open_list.push(&new_state);
-              open_set.insert(new_state);
+              new_state.has_been_visited = true;
+              open_list.push(new_state);
+              open_list_state[new_state.x - 1][new_state.y - 1] = new_state;
             }
           }
         }
       }
     }
-    // std::cout << "open_list size: " << open_list.size() << std::endl;
   }
 }
